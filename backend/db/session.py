efe36ce7c3d50c710beee0a07c6ae50f17ada669
Allen <jihun3353@gmail.com>
2023-11-16 18:43:45 +0900

    update

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,9 +1,7 @@
 from contextvars import ContextVar
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-
 from backend.core.config import settings
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker, async_scoped_session
 
 session_context: ContextVar[str] = ContextVar("session_context")
 
@@ -12,6 +10,6 @@ def get_session_context() -> str:
     return session_context.get()
 
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-ScopedSessionLocal = scoped_session(SessionLocal, scopefunc=get_session_context)
+engine = create_async_engine(settings.SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, pool_recycle=3600)
+SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+ScopedSessionLocal = async_scoped_session(SessionLocal, scopefunc=get_session_context)
