30d5dc96d5e6d73c3e61ba4862f7c2556d88ba89
Allen <jihun3353@gmail.com>
2023-11-17 18:01:29 +0900

    Update docker.entrypoint.init-db.sh

--- scripts/docker.entrypoint.init-db.sh
+++ scripts/docker.entrypoint.init-db.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Creating database: ${POSTGRES_DB}"
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE USER developer;
 	CREATE DATABASE developer;
